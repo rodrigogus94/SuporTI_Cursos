c1ae91bc68349d3420b81aa305c3f4092a4fcb0d
rodrigogus94 <rodrigogus@live.com>
2025-01-09 10:38:46 -0300

    corrigindo a configuração para o static das paginas

--- setup/settings.py
+++ setup/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 from decouple import config, Csv
 from dj_database_url import parse as db_url
 
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
